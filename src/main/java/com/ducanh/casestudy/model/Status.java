64854325c3a62f8ba7da3ee308133dcce6a0e59e
PhuongNamHoang198 <hoangphuongnam.98@gmail.com>
2022-10-03 23:43:17 +0700

    done model player

--- src/main/java/com/ducanh/casestudy/model/Status.java
+++ src/main/java/com/ducanh/casestudy/model/Status.java
@@ -1,4 +1,15 @@
 package com.ducanh.casestudy.model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "status")
 public class Status {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String state;
 }
