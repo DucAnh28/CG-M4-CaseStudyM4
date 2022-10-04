adb9857656f8f2bf8390199391bea2033ed6ed86
Duc Anh Nguyen <ducanhnguyeny2k@gmail.com>
2022-10-04 22:06:47 +0700

    doing homepage with jquery

---
+++ src/main/java/com/ducanh/casestudy/controller/HomeController.java
@@ -0,0 +1,28 @@
+package com.ducanh.casestudy.controller;
+
+import com.ducanh.casestudy.model.Coach;
+import com.ducanh.casestudy.service.coach.ICoachService;
+import com.ducanh.casestudy.service.player.IPlayerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+    @Autowired
+    private IPlayerService playerService;
+    @Autowired
+    private ICoachService coachService;
+
+    @GetMapping("/c")
+    public ResponseEntity<Iterable<Coach>> displayAllCoach() {
+        List<Coach> coaches = (List<Coach>) coachService.findAll();
+        return new ResponseEntity<>(coaches, HttpStatus.OK);
+    }
+}
--- src/main/java/com/ducanh/casestudy/model/Player.java
+++ src/main/java/com/ducanh/casestudy/model/Player.java
@@ -2,6 +2,7 @@ package com.ducanh.casestudy.model;
 
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 
@@ -127,8 +128,6 @@ public class Player {
     public void setAvatarBackGround(String avatarBackGround) {
         this.avatarBackGround = avatarBackGround;
     }
-
-
 
 
     public Position getPosition() {
