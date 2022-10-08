4ecaafa134bc19bf6f59c42c28e86c3eae68142d
Duc Anh Nguyen <ducanhnguyeny2k@gmail.com>
2022-10-08 16:31:07 +0700

    done upload image

---
+++ src/main/java/com/ducanh/casestudy/config/ResourceConfig.java
@@ -0,0 +1,9 @@
+package com.ducanh.casestudy;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ResourceConfig implements WebMvcConfigurer {
+
+}
