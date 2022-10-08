1419b159e77f9a541f6e7951aef2c682583b5a5d
Duc Anh Nguyen <ducanhnguyeny2k@gmail.com>
2022-10-08 16:32:06 +0700

    done upload image

--- src/main/java/com/ducanh/casestudy/config/ResourceConfig.java
+++ src/main/java/com/ducanh/casestudy/config/ResourceConfig.java
@@ -1,9 +1,19 @@
-package com.ducanh.casestudy;
+package com.ducanh.casestudy.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class ResourceConfig implements WebMvcConfigurer {
+    @Value("${upload_file_avatar}")
+    private String fileUpload;
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/image/**")
+                .addResourceLocations("file:" + fileUpload);
+    }
 }
