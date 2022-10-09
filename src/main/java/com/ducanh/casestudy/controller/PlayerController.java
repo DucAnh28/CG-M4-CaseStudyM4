8e40ba15f9530cc2214d67b65e2ada86491178b6
PhuongNamHoang198 <hoangphuongnam.98@gmail.com>
2022-10-09 11:43:03 +0700

    add upload file

--- src/main/java/com/ducanh/casestudy/controller/PlayerController.java
+++ src/main/java/com/ducanh/casestudy/controller/PlayerController.java
@@ -2,23 +2,33 @@ package com.ducanh.casestudy.controller;
 
 import com.ducanh.casestudy.model.*;
 import com.ducanh.casestudy.repository.jwt.IAppUserRepo;
+import com.ducanh.casestudy.service.IGeneralService;
+import com.ducanh.casestudy.service.approle.AppRoleService;
+import com.ducanh.casestudy.service.approle.IAppRoleService;
 import com.ducanh.casestudy.service.appuser.IAppUserService;
 
 import com.ducanh.casestudy.service.player.IPlayerService;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletContext;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 
 @RestController
 @CrossOrigin("*")
@@ -38,7 +48,13 @@ public class PlayerController {
 
     @Autowired
     private IPlayerService playerService;
+    @Autowired
+    private ServletContext servletContext;
+    @Autowired
+    private IAppRoleService appRoleService;
 
+    @Autowired
+    private IAppUserService appUserService;
 
     @GetMapping("/list-player")
     public ResponseEntity<Iterable<Player>> getPlayers() {
@@ -101,11 +117,11 @@ public class PlayerController {
     }
 
 
-    @PostMapping("/create-player")
-    public ResponseEntity<Player> createPlayer(@RequestBody Player player) {
-        playerService.save(player);
-        return new ResponseEntity<>(player, HttpStatus.CREATED);
-    }
+//    @PostMapping("/create-player")
+//    public ResponseEntity<Player> createPlayer(@RequestBody Player player) {
+//        playerService.save(player);
+//        return new ResponseEntity<>(player, HttpStatus.CREATED);
+//    }
 
     @PutMapping("/edit-player/{id}")
     public ResponseEntity<Player> editPlayer(@PathVariable Long id, @RequestBody Player player) {
@@ -126,6 +142,38 @@ public class PlayerController {
         }
         playerService.remove(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PostMapping("create-player")
+
+    public ResponseEntity<Player> addPlayer(@ModelAttribute("player") Player player, @ModelAttribute("avaFile") MultipartFile avaFile) {
+        String path = servletContext.getRealPath("/");
+        System.out.println("path: "+ path);
+        if (avaFile != null) {
+            String avaFileName = avaFile.getOriginalFilename();
+            try {
+                FileCopyUtils.copy(avaFile.getBytes(), new File(upload_file_avatar + avaFileName));
+                player.setAvatarURL("/image/" + avaFileName);
+            } catch (IOException ex) {
+                player.setAvatarURL("image/Error");
+                System.out.println("Loi khi upload File");
+                ex.printStackTrace();
+            }
+        }
+
+        playerService.save(player);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+
+    @RequestMapping(value = "/image/{path}", method = RequestMethod.GET)
+    public ResponseEntity<byte[]> getImageAsResponseEntity(@PathVariable String path) throws IOException {
+        HttpHeaders headers = new HttpHeaders();
+        InputStream in = servletContext.getResourceAsStream(upload_file_avatar+path);
+        byte[] media = IOUtils.toByteArray(in);
+        headers.setCacheControl(CacheControl.noCache().getHeaderValue());
+        ResponseEntity<byte[]> responseEntity = new ResponseEntity<>(media, headers, HttpStatus.OK);
+        return responseEntity;
     }
 
 
